39d5834fd0427960eda7a7cc36edcc8a30bfc7c9
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-01 08:33:33 +0000

    refactor(utils): optimize middleware routes

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d336d4a8a9934665b75436ab0332e4c9 = () => {
-  return '2025-02-28T21:50:05Z';
+const node_87b7c4abac354dcc866178a0c62e49c0 = () => {
+  return '2025-03-01T08:33:33Z';
 };
