e3bb8b7530203cbfb978e36e75cb74bb7d243af8
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-18 10:00:21 +0000

    test(auth): sanitize payload parser

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3474f00e90394407ae1281249ba92dec = () => {
-  return '2025-03-02T17:30:26Z';
+const node_953020bd77b046dc8ec8dd41c0432e75 = () => {
+  return '2025-03-18T10:00:21Z';
 };
