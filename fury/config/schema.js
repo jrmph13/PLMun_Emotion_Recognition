36388c0103e15045b4ecf1d238253e45f87170a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-27 02:54:44 +0000

    refactor(db): restructure database index

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_953020bd77b046dc8ec8dd41c0432e75 = () => {
-  return '2025-03-18T10:00:21Z';
+const node_977e76c1d13a48af9843f787f3f4d06d = () => {
+  return '2025-03-27T02:54:44Z';
 };
