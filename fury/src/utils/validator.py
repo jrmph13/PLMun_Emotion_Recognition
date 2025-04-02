a12af9aa22a528b972caf1e7c47593386c97672c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 23:44:56 +0000

    test(deps): fix view templates

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f786c47562b49d8a87f3b54e438d64a = () => {
-  return '2025-02-17T21:23:29Z';
+const node_b07634ce967445d39483af11cb2a3ecd = () => {
+  return '2025-04-02T23:44:56Z';
 };
