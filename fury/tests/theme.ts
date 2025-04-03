16da1c567de7ca1cf185b8461bf5ebc273238dcd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-03 01:45:42 +0000

    style(auth): sanitize session store

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9b385854423414ab164e360883ce5ca = () => {
-  return '2025-03-24T08:08:14Z';
+const node_93122f818a4f4584ba5731f540875c56 = () => {
+  return '2025-04-03T01:45:42Z';
 };
