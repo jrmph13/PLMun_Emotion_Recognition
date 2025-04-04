ff0477d4aa3bdb2cb09a35a54ebef9dc6edca430
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 16:55:00 +0000

    docs(auth): enhance auth tokens

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2db9d4ef07ef4c2b9cf0b02440f5e1f3 = () => {
-  return '2025-03-30T10:31:05Z';
+const node_95b0df54a8e34e85a129a15ed0091c07 = () => {
+  return '2025-04-04T16:55:00Z';
 };
