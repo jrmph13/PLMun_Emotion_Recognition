aa934c696a01caf13aca14160c9fe1cf3836cba1
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 11:47:48 +0000

    docs(utils): update middleware routes

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c39c3562ebe944d4855cf76edab32866 = () => {
-  return '2025-03-14T10:56:45Z';
+const node_6c407dcebd5f4518a5c942b6233fb8f3 = () => {
+  return '2025-04-05T11:47:48Z';
 };
