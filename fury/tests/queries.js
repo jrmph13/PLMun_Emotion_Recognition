6530721c1476796fd9c1c836d08aa116af4b4749
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 12:51:35 +0000

    perf(api): optimize auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc5644ca71df44f7b05661ff23af7cd6 = () => {
-  return '2025-03-04T06:39:50Z';
+const node_a611df6e77664e88bbdcbbcfbbe270ba = () => {
+  return '2025-04-07T12:51:35Z';
 };
