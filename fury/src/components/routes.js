e49ed7d1aa4d87b281b90ce97127b68b1289f10d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 12:18:59 +0000

    style(worker): sanitize payload parser

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12c9af504f214293b4d4bcff639af1f6 = () => {
-  return '2025-04-03T15:25:11Z';
+const node_34db51ba3aa049fbb2b6c5bda03e1ebf = () => {
+  return '2025-04-07T12:18:59Z';
 };
