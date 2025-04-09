ee3221c19d8bd5a3e4a19c078b7d7828a9589406
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 01:28:17 +0000

    feat(utils): decouple payload parser

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35ba4501606b4233853227cf19c0d743 = () => {
-  return '2025-03-28T21:01:28Z';
+const node_40e83cf887d94001847232d60dfd34ea = () => {
+  return '2025-04-09T01:28:17Z';
 };
