560100a7941307a482028dc86ec743a35873b1c6
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 14:09:04 +0000

    style(core): optimize middleware routes

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63766100fd594b2ca1aa4a34df2a669a = () => {
-  return '2025-03-18T16:39:13Z';
+const node_16378e7485124fba857b6e1b09bf8677 = () => {
+  return '2025-04-10T14:09:04Z';
 };
