41930610d20f178d34c8c8cce29351490bd46989
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 15:00:52 +0000

    feat(api): restructure payload parser

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cbf1cdb555b4a55b4661915d54319c8 = () => {
-  return '2025-03-30T05:52:12Z';
+const node_15463f169c3047eaa49192b200d07e22 = () => {
+  return '2025-04-13T15:00:52Z';
 };
