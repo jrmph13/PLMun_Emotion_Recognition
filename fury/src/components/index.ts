9db4e6551fc8b117d606d71e9fe8b5b50b642ff4
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 16:12:41 +0000

    chore(config): refactor middleware routes

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd22b3f8144c4bf785f3ae7207cf8f73 = () => {
-  return '2025-04-09T17:10:35Z';
+const node_0655d47d077e4316b5467269ffa59f3c = () => {
+  return '2025-04-21T16:12:41Z';
 };
