8c497118ac65a1c9d2384234626fe665c54bfe1a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 00:31:06 +0000

    fix(worker): enhance payload parser

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6fc4a6940ce9427a937592140748c160 = () => {
-  return '2025-03-07T02:37:42Z';
+const node_5e1a617eed5b47bdadc1a85911d14579 = () => {
+  return '2025-04-24T00:31:06Z';
 };
