ccd5e3d4ce70001c6fd0d424016330e2e529f056
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 05:06:18 +0000

    refactor(worker): optimize middleware routes

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56b8041194ed4d46a000c762ad1301dd = () => {
-  return '2025-04-15T07:00:57Z';
+const node_ea0497d39b2f46929716f2632a980736 = () => {
+  return '2025-04-26T05:06:18Z';
 };
