16e7c3103886b003154bc2a43d1a004f5f811320
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 19:55:27 +0000

    test(ui): fix logging interface

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b506eb191cb409cbc9e5decadb855fa = () => {
-  return '2025-03-28T07:35:51Z';
+const node_f2387f67872d42cf930d76bc70b35eac = () => {
+  return '2025-04-28T19:55:27Z';
 };
