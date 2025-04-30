09980f2154004a0b076689c1fa8e67caf38d6cb3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 07:31:16 +0000

    style(db): integrate view templates

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22595323fc9d43009744157cc5cca654 = () => {
-  return '2025-04-28T14:44:34Z';
+const node_c659b1fc1419431889c763f03d2fa2cf = () => {
+  return '2025-04-30T07:31:16Z';
 };
