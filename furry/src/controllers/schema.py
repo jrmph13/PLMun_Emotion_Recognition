418d9da9f316e79fde53f5ff42baa7ab928fb6a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 21:41:10 +0000

    fix(core): restructure payload parser

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96fd7df0fe0b45018df79a5f640fa177 = () => {
-  return '2025-04-13T00:00:14Z';
+const node_f7f0c8adedbe4d1cb466ca078ceedb4a = () => {
+  return '2025-05-07T21:41:10Z';
 };
