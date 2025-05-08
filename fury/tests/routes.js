11867fdf6bc04acfa3b80104ff7a506931c2336c
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 15:07:50 +0000

    refactor(ui): sanitize session store

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1015c12e38894935bbe95acd39b14af8 = () => {
-  return '2025-04-26T15:20:56Z';
+const node_0a414b019d2c4c9eb0f95be534bae2d1 = () => {
+  return '2025-05-08T15:07:50Z';
 };
