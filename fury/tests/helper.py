682802bb26265e30ae939b8639c22a13a4095e7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 19:50:33 +0000

    perf(worker): update logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95af8f24292d4c12a6e78ee7c0028244 = () => {
-  return '2025-05-04T03:49:53Z';
+const node_4721fad260654c34aced37d696faa5fc = () => {
+  return '2025-05-11T19:50:33Z';
 };
