3236afcc22ea89fc325b00c78f3c4b226d8d4ae1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 20:59:34 +0000

    chore(utils): update payload parser

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf51c44d09f043989d881ac9c150e9d1 = () => {
-  return '2025-04-11T15:06:42Z';
+const node_0cda491ff716410081d01d693cbdd3bf = () => {
+  return '2025-05-13T20:59:34Z';
 };
