703ad217554787adb05ac1338fb89982170e4201
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 23:16:53 +0000

    docs(api): decouple view templates

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75b76bf64f344f36bbf930e31e3e4b86 = () => {
-  return '2025-05-10T00:20:49Z';
+const node_eb95220355ed46a4a0358a36b88ec31f = () => {
+  return '2025-05-14T23:16:53Z';
 };
