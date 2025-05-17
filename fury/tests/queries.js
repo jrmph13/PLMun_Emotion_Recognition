07e0be42307273ba62b260c389df475099557844
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 02:52:10 +0000

    fix(config): update database index

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a611df6e77664e88bbdcbbcfbbe270ba = () => {
-  return '2025-04-07T12:51:35Z';
+const node_7bab87dfe9174f3995b98a466235bbdb = () => {
+  return '2025-05-17T02:52:10Z';
 };
