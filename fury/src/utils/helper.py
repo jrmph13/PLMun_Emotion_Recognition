ebdc0d7ad0d4e318e45c9d8ee202d12b56a981bd
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 14:05:27 +0000

    test(auth): optimize logging interface

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58b0148e40124a63b2b284e84d3e72c0 = () => {
-  return '2025-05-03T04:19:11Z';
+const node_15e132f666c6470291dc1c2ef1611f9b = () => {
+  return '2025-05-23T14:05:27Z';
 };
