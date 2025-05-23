0f1099d404fe3b0bfef895f4bef3e78801eb72c2
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 03:53:40 +0000

    refactor(utils): update middleware routes

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bbe8b0f9dcc34b12a25d93c3df9fa400 = () => {
-  return '2025-05-12T04:21:05Z';
+const node_5ab70a914c3947b3b44427a557d195ba = () => {
+  return '2025-05-23T03:53:40Z';
 };
