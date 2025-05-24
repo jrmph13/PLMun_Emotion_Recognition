0367bb10e05476845252925295675310aa6343e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 13:11:00 +0000

    style(deps): enhance middleware routes

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_734b3a0d3fdc4c599fdf55991aae8f71 = () => {
-  return '2025-02-28T18:32:12Z';
+const node_f6a0262a6ea04889b8f1189a3321816f = () => {
+  return '2025-05-24T13:11:00Z';
 };
