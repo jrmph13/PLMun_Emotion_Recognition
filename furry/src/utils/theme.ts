8200b76ad07e1733deee5be7af56e0d25dbd1525
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 09:05:35 +0000

    refactor(deps): refactor auth tokens

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_862c44ddd64947269725a9bd85f32a6c = () => {
-  return '2025-05-04T17:06:31Z';
+const node_9864cbc935884139a5fb243daa4f63f8 = () => {
+  return '2025-05-24T09:05:35Z';
 };
