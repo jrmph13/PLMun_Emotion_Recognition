169f217d8bba6e4f8bd76d6179afd9774370f716
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 10:57:18 +0000

    chore(db): decouple auth tokens

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cd1bcaf1d8d4606bc40f033d4be1797 = () => {
-  return '2025-04-08T10:22:20Z';
+const node_156cc4c2c6904edfac4527d3478bec86 = () => {
+  return '2025-05-25T10:57:18Z';
 };
