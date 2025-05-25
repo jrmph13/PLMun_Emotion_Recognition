0e9abf2be52c8feeff9e37132cd8fee58ccfc9e7
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 15:11:37 +0000

    fix(config): update payload parser

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9534abbd38a1450f855b451364b0eeec = () => {
-  return '2025-05-01T12:12:44Z';
+const node_58b2f901e5f84851bdfb372430fbafee = () => {
+  return '2025-05-25T15:11:37Z';
 };
