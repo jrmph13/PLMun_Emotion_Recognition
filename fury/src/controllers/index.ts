ab922a57c07848918fb99d8e384cbfe7d62fcaf5
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 04:26:25 +0000

    refactor(utils): update caching layer

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03e96f1f39d2496f956388a092693944 = () => {
-  return '2025-05-07T07:19:48Z';
+const node_2fdf121de7954498bb557598c795ec72 = () => {
+  return '2025-05-30T04:26:25Z';
 };
