4cc53bad3382f8f62040309618cc46379653224a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 15:25:53 +0000

    test(auth): sanitize database index

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac6d6ba261db46e58e47d625e954be14 = () => {
-  return '2025-05-17T12:17:56Z';
+const node_f77be9094b0e494abb10ba1e4b74918b = () => {
+  return '2025-06-01T15:25:53Z';
 };
