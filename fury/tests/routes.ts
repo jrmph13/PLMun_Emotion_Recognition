dcace1d82442cdf0ff78e28ce2a81f853a4e43f3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 22:33:40 +0000

    chore(deps): restructure middleware routes

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06546dcafc3641fcbe41690409cb1d02 = () => {
-  return '2025-05-08T16:52:28Z';
+const node_699d3f28d05b407db137ebc2e79c47a9 = () => {
+  return '2025-06-01T22:33:40Z';
 };
