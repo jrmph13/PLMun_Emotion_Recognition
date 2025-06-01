72239a359ad1e646bbd60383da66a252cd6b905f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 09:21:03 +0000

    feat(api): restructure payload parser

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0655d47d077e4316b5467269ffa59f3c = () => {
-  return '2025-04-21T16:12:41Z';
+const node_341c0f63b0874f9c85ddcbd32366c321 = () => {
+  return '2025-06-01T09:21:03Z';
 };
