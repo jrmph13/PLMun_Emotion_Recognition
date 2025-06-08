1a15a6e705da70a8f7566ccdecf91fd1207a6c8e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 00:15:06 +0000

    chore(api): restructure view templates

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aef99262339542298109bcffd32a5df5 = () => {
-  return '2025-04-22T01:59:26Z';
+const node_4d3ae5e283b44666a273874f66cf5181 = () => {
+  return '2025-06-08T00:15:06Z';
 };
