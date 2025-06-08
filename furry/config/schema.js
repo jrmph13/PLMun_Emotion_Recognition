ee406e50cc3fb7d4d8341ea01b1e62877b5fc766
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 10:30:43 +0000

    fix(auth): sanitize auth tokens

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85bafe529c5447809f7b34fc350e6d6e = () => {
-  return '2025-05-02T10:35:20Z';
+const node_da4dd32456574d959ca85ceac082e1aa = () => {
+  return '2025-06-08T10:30:43Z';
 };
