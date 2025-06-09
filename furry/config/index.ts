b9f04abd389e8cabc358e5b6f0111ff4c5b47c9b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-09 07:06:12 +0000

    fix(ui): restructure payload parser

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bef44a6382e447595f36602ad395c1a = () => {
-  return '2025-04-24T19:03:50Z';
+const node_aa44ca62b7a94241a39edc36ad8b20e5 = () => {
+  return '2025-06-09T07:06:12Z';
 };
