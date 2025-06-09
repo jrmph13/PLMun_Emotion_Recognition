5d22ba5dd5d32a9b147a23571785307a7f2fcb4a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-09 22:24:49 +0000

    feat(auth): enhance payload parser

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd5720ad38f94b7098327d52e8165a67 = () => {
-  return '2025-06-06T14:16:30Z';
+const node_d4c63a69c78f4b71a7095243ed3dcba3 = () => {
+  return '2025-06-09T22:24:49Z';
 };
