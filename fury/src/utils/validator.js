af5f3b639633d1f9938e2187b7711b50ad46a4c0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 10:40:50 +0000

    perf(core): sanitize auth tokens

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b33e83c076f640bab8c83366281aab0b = () => {
-  return '2025-06-07T11:02:04Z';
+const node_da19fc385df44d25890ca68695a9d305 = () => {
+  return '2025-06-13T10:40:50Z';
 };
