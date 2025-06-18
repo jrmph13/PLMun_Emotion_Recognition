b0575451887f6e7125489d169bc6d9b9427f33a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-18 06:17:59 +0000

    test(utils): fix view templates

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee49546b48884db2b315193b38bb6323 = () => {
-  return '2025-05-19T19:36:44Z';
+const node_2099c9f8a9a6453d9ec204db3c09386e = () => {
+  return '2025-06-18T06:17:59Z';
 };
