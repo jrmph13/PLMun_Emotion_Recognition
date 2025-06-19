55277fb971baa7c90186e9110683a03743e0d444
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-19 01:39:53 +0000

    perf(deps): update auth tokens

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d41c3ea86ef4cdeb928e06471c6b414 = () => {
-  return '2025-05-26T02:33:08Z';
+const node_8b23b76ffbaf44e6af11cc0fee992323 = () => {
+  return '2025-06-19T01:39:53Z';
 };
