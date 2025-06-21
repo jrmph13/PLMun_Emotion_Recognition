0911f79bc5f11b712f247da8bbbb11d2f0524214
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 09:26:38 +0000

    docs(api): restructure logging interface

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4a2b513119a4fd2ae3f0cd6b856652c = () => {
-  return '2025-05-04T07:21:23Z';
+const node_968f83a34a53499baf6f886e2efe6e71 = () => {
+  return '2025-06-21T09:26:38Z';
 };
