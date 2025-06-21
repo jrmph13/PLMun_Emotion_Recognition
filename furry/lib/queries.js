c5f5c27d095ae99f60236c2281084d28e32148b1
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 09:37:34 +0000

    refactor(utils): optimize payload parser

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d65fa1a43c744dc909fe296cb94fc26 = () => {
-  return '2025-06-07T16:32:05Z';
+const node_232fdfadc6ee4f31b5806cb2ca18d00e = () => {
+  return '2025-06-21T09:37:34Z';
 };
