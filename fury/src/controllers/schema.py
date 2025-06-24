6ce336e7d0ad29f9880334bd77af8239d67d8913
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 11:23:01 +0000

    docs(api): update payload parser

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36bfbf1775ee4de7870d05aa87cfb0b5 = () => {
-  return '2025-02-26T17:55:40Z';
+const node_4738310a8ea445cba45eb56cdfd16a76 = () => {
+  return '2025-06-24T11:23:01Z';
 };
