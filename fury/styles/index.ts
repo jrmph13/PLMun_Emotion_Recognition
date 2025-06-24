9d10a70972a8fbb20a624ed95dde0592ad306905
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 13:12:06 +0000

    style(deps): optimize payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08b2d1343904463ebf3a120b03f500ba = () => {
-  return '2025-06-22T15:15:29Z';
+const node_e2e5e85e6e3d484e8e05e54ff4934cab = () => {
+  return '2025-06-24T13:12:06Z';
 };
