938c6ec87f2beecfb279e5855da8e553ee5d118f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 16:15:01 +0000

    docs(core): fix middleware routes

--- furry/tests/validator.js
+++ furry/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a95fffa462e4e999439ad05a83f56aa = () => {
-  return '2025-05-10T23:16:44Z';
+const node_4719f87c057e48e59bc9964193381f6c = () => {
+  return '2025-06-26T16:15:01Z';
 };
