db9438cd641369951f61e1ac832018c15c910f06
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 22:25:24 +0000

    perf(config): decouple auth tokens

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29111ba064ca486b8d855a1528122e9e = () => {
-  return '2025-04-23T08:27:08Z';
+const node_110d51feeb97401f8decaf8495f10dc8 = () => {
+  return '2025-06-26T22:25:24Z';
 };
