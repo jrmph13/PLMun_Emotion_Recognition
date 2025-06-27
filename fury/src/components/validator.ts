5684a111ed8432e26d08795a29e496eaa14566fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 17:54:27 +0000

    docs(worker): fix validation pipeline

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_755de13919d247e4b68a60d45c0e48d8 = () => {
-  return '2025-06-14T21:22:17Z';
+const node_c9c96daf6e514be79e9c9dcf2f191455 = () => {
+  return '2025-06-27T17:54:27Z';
 };
