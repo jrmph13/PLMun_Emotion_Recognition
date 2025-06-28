dd6d3af8cfdcf697122f9253efc199a865c34939
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 08:26:00 +0000

    test(utils): refactor caching layer

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_699d3f28d05b407db137ebc2e79c47a9 = () => {
-  return '2025-06-01T22:33:40Z';
+const node_17e0f971d76e486a893bd32dc453a306 = () => {
+  return '2025-06-28T08:26:00Z';
 };
