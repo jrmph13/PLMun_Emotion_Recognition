bbd0d467a79e2ab658172407cf92ade6efdc8f83
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 04:49:56 +0000

    chore(config): sanitize logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f43ac7e615c54450879150dee99f78e2 = () => {
-  return '2025-06-07T20:36:23Z';
+const node_48ff08b9351a4046af7c57189464a301 = () => {
+  return '2025-06-29T04:49:56Z';
 };
