57e602e788d2f5664c1ff28fad6ce73e3e679bb0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 08:12:26 +0000

    fix(worker): fix session store

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7b43c51039e413ebd163fc45222fac1 = () => {
-  return '2025-06-26T01:01:28Z';
+const node_03c47009e9094978965818d4ada2d267 = () => {
+  return '2025-06-29T08:12:26Z';
 };
