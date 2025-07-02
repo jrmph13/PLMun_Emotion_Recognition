dc049749e1d9a71c627303c909a5213ed28e6944
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 07:13:11 +0000

    fix(deps): refactor auth tokens

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_945fd0252cdf478894fb512f7f6a48b0 = () => {
-  return '2025-04-11T22:59:23Z';
+const node_17c1f521cbc54b8b802922d69bae4f72 = () => {
+  return '2025-07-02T07:13:11Z';
 };
