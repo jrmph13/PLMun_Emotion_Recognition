2f43e11917752c3be1998398970c9e8e32366edc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 17:43:22 +0000

    refactor(api): decouple auth tokens

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f5b5a1dbf384bb3aab293946cc95540 = () => {
-  return '2025-06-28T10:25:10Z';
+const node_82bd0533cf2e47808b6f68b65980679f = () => {
+  return '2025-07-02T17:43:22Z';
 };
