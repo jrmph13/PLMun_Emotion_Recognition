96ee72383077b4604c42cfa1e96ec1b5e6c66ad0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 08:41:32 +0000

    fix(auth): fix auth tokens

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_458427922c224303932d55431bb266c6 = () => {
-  return '2025-04-21T14:13:28Z';
+const node_3b340da2c4c5452abbb0e4ce32d0f426 = () => {
+  return '2025-07-02T08:41:32Z';
 };
