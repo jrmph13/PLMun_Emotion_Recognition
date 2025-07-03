1049adc2d8daecf1383eafee96cd40800e142fe0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 11:59:44 +0000

    perf(auth): optimize auth tokens

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c58a160a6d2e4b02989f3bc564d00347 = () => {
-  return '2025-05-24T21:12:33Z';
+const node_568507da42f448ca956d3ca990b6edb1 = () => {
+  return '2025-07-03T11:59:44Z';
 };
