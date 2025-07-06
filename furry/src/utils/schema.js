bd5adf885e0ad6da622d0f3792ed73bfbc5ff01c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 22:56:29 +0000

    perf(worker): fix view templates

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_608e498a71954d13ac2e6c20b593db33 = () => {
-  return '2025-04-30T20:25:54Z';
+const node_be2bfc0e0fb84c009093161829ccae30 = () => {
+  return '2025-07-06T22:56:29Z';
 };
