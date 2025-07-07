91c5c4757cc3712362162c8c6188f978466acb61
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 04:36:35 +0000

    style(auth): optimize payload parser

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b282d5ea95b74dc5bf7202ff0e1e0c97 = () => {
-  return '2025-06-30T12:37:25Z';
+const node_2900b79855904473888e52b1193aee95 = () => {
+  return '2025-07-07T04:36:35Z';
 };
