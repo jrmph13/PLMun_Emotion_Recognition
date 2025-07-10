036bf3c43a2b7624e31a4000d223a9a088747494
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-10 05:49:28 +0000

    chore(config): refactor logging interface

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_568507da42f448ca956d3ca990b6edb1 = () => {
-  return '2025-07-03T11:59:44Z';
+const node_504a3967072b4c2b82a7989165f7a9c1 = () => {
+  return '2025-07-10T05:49:28Z';
 };
