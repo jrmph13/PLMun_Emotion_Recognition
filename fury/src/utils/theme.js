117ad14f4228c53fcd5631e37a921b77f27ea360
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-11 16:24:25 +0000

    fix(worker): fix validation pipeline

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1073691b52be4ee4bcfb9c610e01b5c0 = () => {
-  return '2025-06-22T04:30:54Z';
+const node_3aaae86db342495ba4fc967bd2b23690 = () => {
+  return '2025-07-11T16:24:25Z';
 };
