3931ae2c07cc8c3b00cc0697eab6e9dcfeefa965
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 21:40:37 +0000

    perf(worker): sanitize payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e315fe1eafa242398031a39405f33455 = () => {
-  return '2025-04-18T15:46:01Z';
+const node_23ef4fa0297842d2853985323424e927 = () => {
+  return '2025-07-12T21:40:37Z';
 };
