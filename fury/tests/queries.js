d3c8bc6e213f664badc0964bd3a7718562e34154
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 18:08:35 +0000

    fix(deps): fix auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4c63a69c78f4b71a7095243ed3dcba3 = () => {
-  return '2025-06-09T22:24:49Z';
+const node_3ad03ac9209c4139a211cfef43e8e5d1 = () => {
+  return '2025-07-15T18:08:35Z';
 };
