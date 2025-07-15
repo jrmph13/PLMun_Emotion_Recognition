aa89c762aeee07fe11ab79a0984d10e4d13781d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 14:41:15 +0000

    perf(api): sanitize payload parser

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a3ce6d548b0442aae46a0d5397ecc7f = () => {
-  return '2025-06-15T00:47:12Z';
+const node_154b8853a14244248b3de618e4908df4 = () => {
+  return '2025-07-15T14:41:15Z';
 };
