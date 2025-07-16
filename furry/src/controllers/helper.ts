45f74d0713be54e8dff0db8b0a450106f7707a02
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-16 21:29:11 +0000

    style(ui): fix view templates

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ac655314a4e441693db22ebf1d12757 = () => {
-  return '2025-05-28T07:39:39Z';
+const node_0f8d7aee6f0448b4bec99329964e218a = () => {
+  return '2025-07-16T21:29:11Z';
 };
