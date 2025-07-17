1aaa1bde5457b80133970847e6163f78f40cd112
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 18:06:01 +0000

    docs(api): restructure payload parser

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6eb73c6edd464c749d3d20690c71099a = () => {
-  return '2025-07-15T15:50:01Z';
+const node_53dd71d9df8444e18c708bfac59a7ace = () => {
+  return '2025-07-17T18:06:01Z';
 };
