29955c63f56a05de6c8732d6f58d14d7166a2d24
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 12:13:04 +0000

    feat(auth): fix payload parser

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39a133172adb4d59a04a87b55a7e658d = () => {
-  return '2025-07-07T13:54:30Z';
+const node_424872c71eed4272b100fda9290efed3 = () => {
+  return '2025-07-18T12:13:04Z';
 };
