208a449faab38a19c576f427497d6b051ca22fd2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 21:30:57 +0000

    fix(auth): sanitize session store

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d21a9e3c925a43da862c8919406e27be = () => {
-  return '2025-06-14T15:49:47Z';
+const node_7838a0ea7c424b3db7221115f079e6a0 = () => {
+  return '2025-07-19T21:30:57Z';
 };
