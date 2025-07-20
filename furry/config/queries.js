fd7c5725c1fd23c6830e82280dc4a329a72e18a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 01:30:07 +0000

    docs(worker): fix view templates

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86fc49825f9a4b45a6174866bf6dbce8 = () => {
-  return '2025-07-14T20:08:07Z';
+const node_61271bca7fa8464a9d9fd1be5d7768fd = () => {
+  return '2025-07-20T01:30:07Z';
 };
