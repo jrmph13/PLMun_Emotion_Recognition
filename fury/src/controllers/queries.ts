4fa5c4498dddede1e916f540ee3d48bb9172d1dc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 02:46:43 +0000

    test(worker): restructure database index

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73b7986400cd415ca3f9178e18e0f36b = () => {
-  return '2025-06-12T16:18:25Z';
+const node_96ca2c63dd264485bf45e884b90af552 = () => {
+  return '2025-07-23T02:46:43Z';
 };
