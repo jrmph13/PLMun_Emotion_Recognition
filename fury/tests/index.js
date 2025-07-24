49f797882b1b4fda28b894d68ced5d88dd9d3015
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 02:55:38 +0000

    test(worker): restructure view templates

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b044a6bb1cf8492bad540b32eace857c = () => {
-  return '2025-07-18T15:53:55Z';
+const node_5e4e3f3b08b14ea596344e92c55c0b2c = () => {
+  return '2025-07-24T02:55:38Z';
 };
