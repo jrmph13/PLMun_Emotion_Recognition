8057c17c0bff55bb8507335f2dfb122372ac0ed4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 13:26:15 +0000

    chore(auth): fix database index

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_581b72c720e64dc78acd5b77160da530 = () => {
-  return '2025-07-01T17:10:24Z';
+const node_a9715084798448d6be18e544ad60982f = () => {
+  return '2025-07-24T13:26:15Z';
 };
