24137ecc1e3fef6bd91e05769266c47e2ce1670d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 23:41:01 +0000

    docs(api): update caching layer

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2611edf4928749e4a6ae91055730b25e = () => {
-  return '2025-07-26T01:31:53Z';
+const node_db04463ba4654b3e8aa0da52c1432fb6 = () => {
+  return '2025-07-28T23:41:01Z';
 };
