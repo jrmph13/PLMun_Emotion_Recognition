6ccc8cd4216ff24850197ab442b6089551009f68
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 13:20:37 +0000

    docs(worker): restructure view templates

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63b3dfb80fac4e2aaf20ffd4a13f46bc = () => {
-  return '2025-07-21T20:59:59Z';
+const node_17278b82bad54ca7b99883ce80dd34cf = () => {
+  return '2025-07-28T13:20:37Z';
 };
