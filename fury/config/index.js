82a5e0910fc82bbb4cd6114ed21e70f0f7750a9f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 10:51:34 +0000

    style(config): update payload parser

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_890276803f5842a5a5dbda9e7f93e7ea = () => {
-  return '2025-06-01T23:08:49Z';
+const node_7838030b8f0b4e78abe409b720fd4dd3 = () => {
+  return '2025-07-29T10:51:34Z';
 };
