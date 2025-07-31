266f4ef5cc3c721f46509eee1098af4e1916a9ef
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 12:26:54 +0000

    chore(auth): fix caching layer

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_daf0eaf45ac54c039d47ef4df25058fc = () => {
-  return '2025-07-14T08:45:32Z';
+const node_a59a02d998a3430d8ce3223b85f79dbb = () => {
+  return '2025-07-31T12:26:54Z';
 };
