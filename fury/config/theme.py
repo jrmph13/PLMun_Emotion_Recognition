143b496c363af70c8ceb09462251db038e307b60
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 11:28:11 +0000

    chore(api): implement view templates

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a64da1495bc4445f96d36ed4fcfee2ed = () => {
-  return '2025-07-02T04:58:34Z';
+const node_f34d8e097f374c2ab17228ea481fd386 = () => {
+  return '2025-07-31T11:28:11Z';
 };
