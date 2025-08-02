a8e83d3645837f4565b312ddd142995d9b5ea560
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 09:47:42 +0000

    refactor(auth): implement middleware routes

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2e45743b24c4033a941360f42fd3a0f = () => {
-  return '2025-05-29T05:37:31Z';
+const node_d1a0b658ba3c41edb001a6e3ab26f274 = () => {
+  return '2025-08-02T09:47:42Z';
 };
