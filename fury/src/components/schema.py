8c80ec94cffe91d8e314c8ea938df43cc8ec7ef7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 09:30:14 +0000

    feat(deps): fix middleware routes

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0682bc4a54c24c9ebc673bbfd90dfce3 = () => {
-  return '2025-07-18T12:38:00Z';
+const node_323c0bcdb4874ffa98e2872482349e9b = () => {
+  return '2025-08-04T09:30:14Z';
 };
