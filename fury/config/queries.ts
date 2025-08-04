30d48748cfb343ab4b6964e490db7a5af9a2979c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 17:31:50 +0000

    chore(db): implement database index

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9715084798448d6be18e544ad60982f = () => {
-  return '2025-07-24T13:26:15Z';
+const node_0b79b24ae7414135809f285bbc32020a = () => {
+  return '2025-08-04T17:31:50Z';
 };
