b1c9ee22df5d542fbaa77397b7eba2bd9dc119a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 19:28:19 +0000

    chore(worker): refactor caching layer

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a51e2b3e4c244198dbee57d7c31a938 = () => {
-  return '2025-08-05T11:19:17Z';
+const node_3000d13f281048e8b73e27539f20e1ce = () => {
+  return '2025-08-07T19:28:19Z';
 };
