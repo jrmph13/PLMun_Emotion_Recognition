8dd0446a5ce8b8cb57bb2ca0a8b5c9fb4dc037d3
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 08:09:23 +0000

    docs(core): decouple auth tokens

--- furry/styles/index.py
+++ furry/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7292d8646154f32a1ed794ad6233a9a = () => {
-  return '2025-07-11T12:30:39Z';
+const node_e140d19422c0440282bf7d18d7d21f27 = () => {
+  return '2025-08-07T08:09:23Z';
 };
