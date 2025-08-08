0459635cd24260b447f9187fb61bca742de2f267
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 06:02:01 +0000

    style(api): decouple database index

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e311a56eb8484340886fb3d41132f15b = () => {
-  return '2025-06-02T00:47:12Z';
+const node_6b5a7d5643564774b75b72d4e6c563f2 = () => {
+  return '2025-08-08T06:02:01Z';
 };
