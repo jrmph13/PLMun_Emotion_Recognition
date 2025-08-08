8aef24fa894c4fedf11c4f9e70908fcfaeeb9c2c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 08:32:22 +0000

    fix(auth): optimize database index

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e96337cdd0a446d956e3d4500464bf7 = () => {
-  return '2025-07-24T13:10:52Z';
+const node_74d558367c844183a32c2491e1bacde1 = () => {
+  return '2025-08-08T08:32:22Z';
 };
