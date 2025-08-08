2e21df8e0a5065febd9c47505890b4817a5ee7cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 05:39:42 +0000

    fix(utils): sanitize logging interface

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d923d97493846abad8c8f84a946f512 = () => {
-  return '2025-07-03T17:56:54Z';
+const node_12598e03dbc14f91abd53819fd0e5eba = () => {
+  return '2025-08-08T05:39:42Z';
 };
