1dd2f6e0e9459f023a55e3db9575f66c5f09acbd
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 11:01:12 +0000

    test(auth): optimize logging interface

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14f4a062d0e14183bd7135daac53fe6c = () => {
-  return '2025-07-23T18:52:10Z';
+const node_229d0ae1a19b43c9a6b11d82e5df8686 = () => {
+  return '2025-08-11T11:01:12Z';
 };
