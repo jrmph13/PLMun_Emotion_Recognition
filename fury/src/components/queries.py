a27fb2389e6289785502c9aa6d6523bb59086e8f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 18:15:58 +0000

    fix(core): integrate database index

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12598e03dbc14f91abd53819fd0e5eba = () => {
-  return '2025-08-08T05:39:42Z';
+const node_6dd789160f8a47ddb1366434808d5e7d = () => {
+  return '2025-08-15T18:15:58Z';
 };
