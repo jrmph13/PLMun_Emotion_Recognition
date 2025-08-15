0673227d7ff994c5167422daa7ad2298a9eee76b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 14:03:08 +0000

    refactor(deps): sanitize session store

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40be3d82e6b84b968d289e81e97d2cf2 = () => {
-  return '2025-07-19T09:57:30Z';
+const node_9dcb2e6dc08946c6ac41b76d39d98f9e = () => {
+  return '2025-08-15T14:03:08Z';
 };
