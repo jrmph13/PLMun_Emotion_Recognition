065b2bc1c8c2db07c8726e930b423e9fe6b8e9b7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 01:29:20 +0000

    test(ui): update auth tokens

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aeb068b2360f48efb77cb701424e68c6 = () => {
-  return '2025-07-25T14:41:43Z';
+const node_310d4998c8a44600bada56b8f3ce1437 = () => {
+  return '2025-08-22T01:29:20Z';
 };
