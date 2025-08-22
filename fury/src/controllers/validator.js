49b056fe3d34f9eab7eb85f1e7c9583f8fb18ef7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 06:21:44 +0000

    perf(api): fix auth tokens

--- fury/src/controllers/validator.js
+++ fury/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53348c5f603f435e8819c91b9e1b197b = () => {
-  return '2025-08-04T03:19:46Z';
+const node_ae37eefb4fae4e6f834efc9841921b0e = () => {
+  return '2025-08-22T06:21:44Z';
 };
