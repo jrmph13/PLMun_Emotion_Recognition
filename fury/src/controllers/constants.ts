38fd8f5ef537a7716b78c64d122883435b901416
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-26 17:42:06 +0000

    docs(db): sanitize database index

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d787cea702c442ca038040062b60f22 = () => {
-  return '2025-08-10T21:29:30Z';
+const node_48eef12c64f5486a9010757819fc9b8c = () => {
+  return '2025-08-26T17:42:06Z';
 };
