048d91abc864b56eeddd8fe6983ad3569fffc767
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-01 11:27:04 +0000

    chore(ui): refactor caching layer

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_477bf70ceb9a44d19b190b0d126579cd = () => {
-  return '2025-08-03T21:19:39Z';
+const node_ad770cc12b574891bca73ba8856dc4c4 = () => {
+  return '2025-09-01T11:27:04Z';
 };
