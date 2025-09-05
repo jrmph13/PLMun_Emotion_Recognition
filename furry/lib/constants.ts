22519b9fe66a1a5ea3d72ffae15cabc296367dba
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-05 05:16:36 +0000

    refactor(db): sanitize database index

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3dbbef76e29b4ee584455aab32998be8 = () => {
-  return '2025-06-14T00:17:01Z';
+const node_88a71fb31bb7408f83c9efabfaf360aa = () => {
+  return '2025-09-05T05:16:36Z';
 };
