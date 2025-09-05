0028ee8f5d0e06819fb274b1c98e692d0850ba6c
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-05 19:11:40 +0000

    docs(api): implement auth tokens

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ad549c0f5a347cd90afe9da74d55161 = () => {
-  return '2025-08-10T19:22:33Z';
+const node_0a05a834f83c4a4fbd080754ba1bc972 = () => {
+  return '2025-09-05T19:11:40Z';
 };
