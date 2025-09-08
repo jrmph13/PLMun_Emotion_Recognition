05d302fdf80fb4856c2a7b6c80daf14dfbe73415
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 03:57:04 +0000

    test(ui): decouple caching layer

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_824d15286322430faa2e54e40ca6c5cd = () => {
-  return '2025-06-14T19:57:04Z';
+const node_fa5e48cdb6b24fbe9ee20b2d22aae70b = () => {
+  return '2025-09-08T03:57:04Z';
 };
