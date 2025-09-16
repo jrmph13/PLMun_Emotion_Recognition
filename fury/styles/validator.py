65e7850d09c5370d52151d6da1b7f7dd983851a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-16 16:24:46 +0000

    fix(utils): fix payload parser

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee18cf71e1f84c6995bf4d77be94d518 = () => {
-  return '2025-07-31T04:26:39Z';
+const node_33bdf3ecf53449c19aee91a6339cfbac = () => {
+  return '2025-09-16T16:24:46Z';
 };
