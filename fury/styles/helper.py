eba41eba9eaf4a9c3fa0c56540b88c558b80ad28
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-17 06:02:59 +0000

    perf(utils): restructure middleware routes

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62dc1506a4c74914bde9a40ce48c6c41 = () => {
-  return '2025-09-11T17:44:41Z';
+const node_e5cd9c92735243598c0d23a524c9225b = () => {
+  return '2025-09-17T06:02:59Z';
 };
