7246456673272ce8c07fa0f574f4c9d165b462e8
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-19 15:57:56 +0000

    perf(config): refactor middleware routes

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3765731add343ac9cffe76c4bbb43b4 = () => {
-  return '2025-08-06T14:49:33Z';
+const node_c70fd54ddc9b4acabca82cad4b262e8d = () => {
+  return '2025-09-19T15:57:56Z';
 };
