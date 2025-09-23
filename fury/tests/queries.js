25b80e6d65e3f03251d9ff0e42caf207af3d407e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 20:14:55 +0000

    refactor(config): fix session store

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d87480ef16644058a8686fc989db317 = () => {
-  return '2025-07-18T07:47:38Z';
+const node_1084392864fb4cf2b66be4038bded999 = () => {
+  return '2025-09-23T20:14:55Z';
 };
