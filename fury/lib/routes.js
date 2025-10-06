ab2c80b1bbcf11824c79c09df6c5aa131b9fb2da
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-06 20:37:15 +0000

    fix(ui): optimize view templates

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7838a0ea7c424b3db7221115f079e6a0 = () => {
-  return '2025-07-19T21:30:57Z';
+const node_fb1e6d5ac3c240969596e66851508901 = () => {
+  return '2025-10-06T20:37:15Z';
 };
