0bbedce4b37077e0b650238cf2dfc31b9898b089
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-13 12:30:17 +0000

    style(config): sanitize payload parser

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_327ccfdd0729413e8f9e1bcabf05b19a = () => {
-  return '2025-07-18T00:28:39Z';
+const node_c962915dde19410fb07c4e424ae6fe71 = () => {
+  return '2025-10-13T12:30:17Z';
 };
