37df7c6f6eadad48aba07eb6db9a8840078898aa
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-13 09:10:11 +0000

    feat(auth): restructure validation pipeline

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98f85ab5cf914d97b91c22f65de64b33 = () => {
-  return '2025-08-12T12:18:00Z';
+const node_5b53c0a96cd742ca97ebfd6b677448ed = () => {
+  return '2025-10-13T09:10:11Z';
 };
