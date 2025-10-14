f8b9b204f9c8be755d0ededc2c036f51f978efb6
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-14 22:59:08 +0000

    fix(utils): decouple database index

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f83f560e369d4c0b927b85250cd40e49 = () => {
-  return '2025-06-30T23:48:23Z';
+const node_15dff1f68a594709a2cab0b6e919b915 = () => {
+  return '2025-10-14T22:59:08Z';
 };
