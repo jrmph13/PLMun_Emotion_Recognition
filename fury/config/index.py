2921ba62a8552bec98ec89a98b7f676d87d7e9b0
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-16 12:43:08 +0000

    fix(auth): implement view templates

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2e1a287007643c1afac14e402235f21 = () => {
-  return '2025-08-10T00:09:24Z';
+const node_256806c4cb9b40b3bbebce0c1180bf10 = () => {
+  return '2025-10-16T12:43:08Z';
 };
