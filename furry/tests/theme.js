1ff7456f383aae3cbf457e08fefad98597774d08
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-21 10:54:48 +0000

    feat(core): update auth tokens

--- furry/tests/theme.js
+++ furry/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd223e0ecfe1450a84739add3a36eb7d = () => {
-  return '2025-08-09T08:20:21Z';
+const node_8ceddd14017f422190794dca9a080d58 = () => {
+  return '2025-10-21T10:54:48Z';
 };
