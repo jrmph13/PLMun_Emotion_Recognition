4ad47b66e3753ed321ac9157748a95a4033d2f32
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-29 08:55:54 +0000

    refactor(worker): update auth tokens

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9abb25011ff447bba1f0f68950360a18 = () => {
-  return '2025-06-14T04:02:18Z';
+const node_d390b20eb63648ca906af4431a94e024 = () => {
+  return '2025-10-29T08:55:54Z';
 };
