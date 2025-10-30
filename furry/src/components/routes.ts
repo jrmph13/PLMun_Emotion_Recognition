e0ba0e05aaba192a2d88471838c7d640a15adf8d
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-30 13:45:16 +0000

    fix(ui): sanitize middleware routes

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_295b7cb6cffd45acba0c5f76e85228b1 = () => {
-  return '2025-06-24T10:59:34Z';
+const node_0932261294b94f8ab60ce7d253628610 = () => {
+  return '2025-10-30T13:45:16Z';
 };
