42f56171e6bf294a3f2ab7557d821dfb51d77311
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 18:44:31 +0000

    feat(worker): sanitize database index

--- furry/src/components/constants.js
+++ furry/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f37455312f9490db96269dd6369ca50 = () => {
-  return '2025-07-06T00:13:43Z';
+const node_cbabadbd1242410eb9b32dbed19c75e4 = () => {
+  return '2025-11-14T18:44:31Z';
 };
