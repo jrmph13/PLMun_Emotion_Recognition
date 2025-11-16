48ae1bd989b7612d76e060552a0376d037cffe38
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 20:17:57 +0000

    perf(auth): implement auth tokens

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3000d13f281048e8b73e27539f20e1ce = () => {
-  return '2025-08-07T19:28:19Z';
+const node_a9539058d8054817a666162a646165a6 = () => {
+  return '2025-11-16T20:17:57Z';
 };
