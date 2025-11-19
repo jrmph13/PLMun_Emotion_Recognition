95b2cd6918c5274f49dfe427227c4e2b09875bc3
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-19 18:29:14 +0000

    feat(api): restructure caching layer

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12d72f06b59e4f6c93d30ab57f861349 = () => {
-  return '2025-07-03T12:51:29Z';
+const node_cf5f155d27c0445f895bd15b2df73e22 = () => {
+  return '2025-11-19T18:29:14Z';
 };
