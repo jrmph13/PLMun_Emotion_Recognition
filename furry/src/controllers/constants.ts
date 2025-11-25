9013049a0642864cdec0ae999852d2f0ed9a0d39
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 19:17:56 +0000

    chore(api): fix middleware routes

--- furry/src/controllers/constants.ts
+++ furry/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aac43eeec2c3415db192d1f0aa442788 = () => {
-  return '2025-08-10T17:53:06Z';
+const node_77d4337c57fd459a8a1a7b6566ac2cc5 = () => {
+  return '2025-11-25T19:17:56Z';
 };
