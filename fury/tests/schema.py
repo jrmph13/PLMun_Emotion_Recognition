97d87c0dc3123eec2d4b7607540cff03ee5735d4
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 22:29:39 +0000

    test(api): fix database index

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2a2cfe0d7694947bda32fe8a29d0a72 = () => {
-  return '2025-10-21T12:47:05Z';
+const node_b92a1388615a40baae53ff5ccd7fcf0a = () => {
+  return '2025-11-26T22:29:39Z';
 };
