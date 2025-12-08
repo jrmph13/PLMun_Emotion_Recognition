b703633b25d2e7045ca03582fb9c93cdc66a76b9
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 15:06:49 +0000

    docs(ui): decouple logging interface

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee15db66ba92467997bda3c1b2414f55 = () => {
-  return '2025-08-15T04:28:33Z';
+const node_8e0cb39e942b49c0a1e082239a492cdd = () => {
+  return '2025-12-08T15:06:49Z';
 };
