3edcade3072c3655973c93f3d778c759b187d1d0
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 04:02:05 +0000

    perf(core): enhance payload parser

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5efba860b9e142d8bbb8b885823e7ff9 = () => {
-  return '2025-08-02T09:40:14Z';
+const node_952bcadf00eb419e928734f98552809c = () => {
+  return '2025-12-08T04:02:05Z';
 };
