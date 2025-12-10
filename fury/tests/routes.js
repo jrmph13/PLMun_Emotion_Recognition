1750bcc214395617b5027115e917996a16832bf1
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-10 17:50:41 +0000

    refactor(core): implement middleware routes

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5894fff50fe4d969a2c31ef9d799cdc = () => {
-  return '2025-11-16T08:48:37Z';
+const node_1abc5de69fdf49d69353e8e2a788eea8 = () => {
+  return '2025-12-10T17:50:41Z';
 };
