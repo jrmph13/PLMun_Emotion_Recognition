f3122024c6053d0aa066a57826a03831e39f6c05
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 22:55:05 +0000

    fix(db): fix session store

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4441fb933a2d4b83931cf6472689b587 = () => {
-  return '2025-10-13T16:11:41Z';
+const node_524713a99b7d4523b5ec1191d7e4b327 = () => {
+  return '2025-12-15T22:55:05Z';
 };
