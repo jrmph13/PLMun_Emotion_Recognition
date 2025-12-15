1ca0cd75223119428e2e29cebb4d3559c0259897
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 16:47:11 +0000

    feat(core): fix logging interface

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8f45229161247af9b4841eb79e7ebda = () => {
-  return '2025-11-24T11:54:31Z';
+const node_5aace674d886404ea97d9d001951b96c = () => {
+  return '2025-12-15T16:47:11Z';
 };
