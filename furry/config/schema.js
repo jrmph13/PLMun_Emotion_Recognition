39fd222d77e5e8a07f00d34e087b1f2fbc55485b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 20:16:52 +0000

    test(api): enhance auth tokens

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e085a2e25d724b388dbae9bda2496cb0 = () => {
-  return '2025-09-17T18:54:59Z';
+const node_b60ac1a8aa7d48cea93f680f3971cb3b = () => {
+  return '2025-12-16T20:16:52Z';
 };
