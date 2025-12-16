c03e9eba40887a880339a1e51c9d9b3c0010b1f0
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 02:17:39 +0000

    perf(utils): sanitize view templates

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d10b3be888a74ad0a437936df2be9a2e = () => {
-  return '2025-08-30T19:00:56Z';
+const node_c6e57769b57a496e94218cd1fabd940a = () => {
+  return '2025-12-16T02:17:39Z';
 };
