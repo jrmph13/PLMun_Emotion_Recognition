65b83560117248d2c7f6130284101ba6b74dbcc8
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-19 10:38:12 +0000

    chore(api): sanitize session store

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21f5314ebd6a422184f0bac11cea4b16 = () => {
-  return '2025-10-26T21:21:44Z';
+const node_f62eb83267814828ac06ea816bf38dd6 = () => {
+  return '2025-12-19T10:38:12Z';
 };
