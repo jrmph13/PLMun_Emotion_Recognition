8052022c242685efb63e9b48fa30cac410d566ec
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 10:25:06 +0000

    fix(api): fix middleware routes

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f97e1bb632a483c886688f3cb53a047 = () => {
-  return '2025-08-03T09:19:32Z';
+const node_1827442deab74022996c93f08f90eb29 = () => {
+  return '2025-12-25T10:25:06Z';
 };
