dc62fef21956e4db4b1a1383cae868398815e557
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 18:45:59 +0000

    chore(utils): update logging interface

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df00d646109e444e9176dbdcf6cec8d3 = () => {
-  return '2025-12-25T01:31:20Z';
+const node_1cb8c8048e2f4f20bc67005a882538bf = () => {
+  return '2025-12-28T18:45:59Z';
 };
