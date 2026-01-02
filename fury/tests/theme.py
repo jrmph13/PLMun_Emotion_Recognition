056f993cfd0b64f84937e50bad5092bdf8e613f6
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-02 22:08:05 +0000

    feat(core): decouple auth tokens

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c34e049c846e48a192034e7108db9228 = () => {
-  return '2025-09-25T07:19:07Z';
+const node_2df21c566f4e41b59ccb3a02f077e956 = () => {
+  return '2026-01-02T22:08:05Z';
 };
