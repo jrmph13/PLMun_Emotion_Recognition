38d76466e94c2e5d7978f842f695786cd304507e
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-07 22:58:07 +0000

    perf(auth): decouple middleware routes

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed4d1c3af23c4173a3b385c05840d30e = () => {
-  return '2025-11-30T23:38:37Z';
+const node_71510e687d614bf4bcb434b5f8f924c4 = () => {
+  return '2026-01-07T22:58:07Z';
 };
