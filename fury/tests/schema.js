dfbf208f39321c9782bfe4aeca38d103f5285b9a
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-07 08:44:28 +0000

    feat(ui): restructure logging interface

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6975117ce8d041308fada92d661ab307 = () => {
-  return '2025-09-17T16:42:13Z';
+const node_c2dd8c9cbe3a46e0b6573b31240e27d0 = () => {
+  return '2026-01-07T08:44:28Z';
 };
