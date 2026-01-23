260956105219f3602eed616875b5fdb0cc9f1390
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-23 21:34:20 +0000

    style(config): update payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e41d46a17f694292b2f4e3bd513f2b61 = () => {
-  return '2025-11-25T16:03:47Z';
+const node_147e411894694c53b8ea805379415009 = () => {
+  return '2026-01-23T21:34:20Z';
 };
