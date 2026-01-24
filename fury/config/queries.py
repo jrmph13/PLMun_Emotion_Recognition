0b6e152f619dbe6e713e13db279eb582d12993dd
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 07:45:23 +0000

    chore(worker): fix auth tokens

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_162890a654d5466bb7802373954abac8 = () => {
-  return '2025-09-14T08:35:29Z';
+const node_ea07faab902b43ddbeb90c25f880316d = () => {
+  return '2026-01-24T07:45:23Z';
 };
