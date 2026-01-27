5d7670acb9f29c867baed8762a6198a66d0008f0
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 11:17:42 +0000

    perf(deps): optimize view templates

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb83f1be10464dfaa8522cdae310f653 = () => {
-  return '2025-08-02T00:47:07Z';
+const node_a8dc7b5ee2264ac2ba1c1af0c928ce0c = () => {
+  return '2026-01-27T11:17:42Z';
 };
