3842b28e409d0669b638f7937d8b947071870fc4
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 03:13:49 +0000

    test(api): update database index

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2dd8c9cbe3a46e0b6573b31240e27d0 = () => {
-  return '2026-01-07T08:44:28Z';
+const node_3c5f25b3e9324a3a89412380e6fe97da = () => {
+  return '2026-02-02T03:13:49Z';
 };
