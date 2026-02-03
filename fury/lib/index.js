c5c646a79c3f173ad1bc3c0792dae22bdced2c5d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 17:11:06 +0000

    fix(core): enhance payload parser

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_819c197ec94b401a9429d289f96973d6 = () => {
-  return '2026-01-16T04:42:02Z';
+const node_7320adaa7a3544f7bce815431d926928 = () => {
+  return '2026-02-03T17:11:06Z';
 };
