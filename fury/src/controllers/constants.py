924d8225212551396460e06992187f1368fe869f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 00:41:17 +0000

    refactor(deps): update payload parser

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ad3538c4ada4654a69a4b4720b89386 = () => {
-  return '2026-01-10T22:24:32Z';
+const node_be32d60279d14e1b890e6ddae5624bef = () => {
+  return '2026-02-03T00:41:17Z';
 };
