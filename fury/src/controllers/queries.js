5b85e6840f7aa44e52a141d7ee6d3bb949ab125d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 05:55:12 +0000

    style(auth): restructure database index

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba8a0146778e4129adc71a8780c5f91e = () => {
-  return '2026-02-04T05:28:24Z';
+const node_2f62d09c268049e5a3cf8e5232d745e2 = () => {
+  return '2026-02-05T05:55:12Z';
 };
