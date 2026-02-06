043af0b0544a847c590f123fafc2d5ca060df99c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 00:08:51 +0000

    test(worker): decouple payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f51996e0d5b44c1bcdad443f2d5cb4a = () => {
-  return '2026-01-24T08:29:27Z';
+const node_5392d2651965485fb17d2d63e341f53b = () => {
+  return '2026-02-06T00:08:51Z';
 };
