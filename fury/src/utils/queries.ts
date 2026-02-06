5aa28c3f96910de374d554fbd805bd30313bfa5c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 11:19:53 +0000

    style(api): integrate view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d03c6c204f28406aac33d39f35e292e9 = () => {
-  return '2025-07-21T07:54:27Z';
+const node_434bc846c8e74146a704abcac808a5f9 = () => {
+  return '2026-02-06T11:19:53Z';
 };
