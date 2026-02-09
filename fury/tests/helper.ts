7064e690afd70f66e11951e269471f8ef24b5f40
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 22:22:07 +0000

    refactor(ui): enhance middleware routes

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fbf3922b01c4b10ad394c72e057d0a0 = () => {
-  return '2025-08-12T07:58:39Z';
+const node_2352ffba7a384ad2a105a34644138e96 = () => {
+  return '2026-02-09T22:22:07Z';
 };
