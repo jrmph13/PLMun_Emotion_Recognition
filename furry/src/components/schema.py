80f8143547da95b73c9852a2b15cbd92cd59027b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 12:40:41 +0000

    fix(core): fix middleware routes

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bcc009da9084ca0b50e7eeea8cd19e1 = () => {
-  return '2026-02-02T23:54:08Z';
+const node_cc92746e59e74ef5b7f615346b60936e = () => {
+  return '2026-02-09T12:40:41Z';
 };
