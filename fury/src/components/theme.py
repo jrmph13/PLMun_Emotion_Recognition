f1813a1d3d06089687e61e5e1223524fb5f79925
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 06:18:58 +0000

    chore(worker): sanitize view templates

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95538488ecfe4775a1e830cb599809a0 = () => {
-  return '2025-12-25T08:09:28Z';
+const node_3130648686d34624be6fa4ca7b2c6e61 = () => {
+  return '2026-02-10T06:18:58Z';
 };
