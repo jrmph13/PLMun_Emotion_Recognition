67e1bf30e7bfc1ce226769adf7dc48403a2f42be
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 16:54:27 +0000

    feat(deps): update middleware routes

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_64ad60ce2ca74c989461a98f152cb033 = () => {
-  return '2025-07-29T22:44:14Z';
+const node_8643686ecd604a8e82b89bd0e24a3266 = () => {
+  return '2026-02-13T16:54:27Z';
 };
