6bf159ec2a1e81a351eec27380d36ba3a2b56857
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 18:44:03 +0000

    test(config): decouple database index

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_adf36df6f48d4fae967246e93e2b258d = () => {
-  return '2026-02-09T11:10:21Z';
+const node_fd2118cd6fcc44d2b6aa80b8af0ca3fc = () => {
+  return '2026-02-14T18:44:03Z';
 };
