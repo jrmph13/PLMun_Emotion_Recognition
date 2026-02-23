06f3f806ed47510c06ac6771e51b3b1f190d7bed
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 16:41:59 +0000

    style(ui): restructure session store

--- furry/styles/constants.js
+++ furry/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb23b3c82622403fb13f458b3effeada = () => {
-  return '2026-01-27T10:26:14Z';
+const node_0b76e76958fe4de3981d0e1ca0b132a1 = () => {
+  return '2026-02-23T16:41:59Z';
 };
