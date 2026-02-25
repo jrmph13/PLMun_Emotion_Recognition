31ae9292cb8e85248ff0a43780e6f13b6e257074
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 15:45:30 +0000

    fix(ui): integrate view templates

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8dc7b5ee2264ac2ba1c1af0c928ce0c = () => {
-  return '2026-01-27T11:17:42Z';
+const node_83a959e0f4224569b0dfcc5d0e7f5a37 = () => {
+  return '2026-02-25T15:45:30Z';
 };
