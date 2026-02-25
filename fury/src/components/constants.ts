23265febf5758ffcc77150cdae4f2f85bd29af4c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 09:06:24 +0000

    feat(core): enhance middleware routes

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f205a53d08ed4ae18ab63ec0c00d47c1 = () => {
-  return '2026-02-13T22:37:41Z';
+const node_60252b24fa6e49b991416ecdc1f6e17f = () => {
+  return '2026-02-25T09:06:24Z';
 };
