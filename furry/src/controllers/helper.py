af4bda66336bed06510691782fbb21048dfc51c5
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 14:00:09 +0000

    docs(ui): integrate auth tokens

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b29a5aba9c6046ff83bc856c9fba73d8 = () => {
-  return '2026-01-30T10:53:21Z';
+const node_b44cc39c026d493c840664cddc757407 = () => {
+  return '2026-03-06T14:00:09Z';
 };
