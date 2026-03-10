11d7cd2c9ed46537a179590ae22c0ba34a4098aa
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 23:11:22 +0000

    perf(core): update middleware routes

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44d87072a40c405da4e8c4a8f22864a5 = () => {
-  return '2026-03-08T23:29:07Z';
+const node_d8c1bcd981b643b986bf995319f48a14 = () => {
+  return '2026-03-10T23:11:22Z';
 };
