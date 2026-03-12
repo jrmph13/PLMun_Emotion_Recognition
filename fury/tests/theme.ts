61befcdd38cc46bb1aa34fd96645aa905f5f1e0d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 23:54:37 +0000

    style(config): fix middleware routes

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d712cd0959914cda8c3d526157f09dfc = () => {
-  return '2026-02-18T06:37:09Z';
+const node_b7e400de9600412f90b1e9e4d828d6ed = () => {
+  return '2026-03-12T23:54:37Z';
 };
