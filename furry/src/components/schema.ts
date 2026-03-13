e7ea4c833eccf65a0b8c55ec37e7080e1445c0f2
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 16:44:56 +0000

    perf(core): decouple middleware routes

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03eb6025667543778217943c5aa84998 = () => {
-  return '2025-05-29T01:22:33Z';
+const node_001a44d448e241288edefde84e45203d = () => {
+  return '2026-03-13T16:44:56Z';
 };
