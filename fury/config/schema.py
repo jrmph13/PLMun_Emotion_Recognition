10603baca26e71caaba1498a43c98e54d12cf459
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 01:26:24 +0000

    test(deps): fix payload parser

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c8a868fa16c439ea09d6a960cf280ca = () => {
-  return '2026-01-05T06:00:26Z';
+const node_2245a032f9a540a1a1f29fb09c8c4666 = () => {
+  return '2026-03-14T01:26:24Z';
 };
