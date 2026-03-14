1500424ef8c8ac9866c99e32b7b665988dd39d24
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 04:56:22 +0000

    test(utils): sanitize middleware routes

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf8a7d70a2c54b1ebbaa0f1c6727238c = () => {
-  return '2026-02-17T12:06:52Z';
+const node_e77264025dac496989689c0a11d7b05d = () => {
+  return '2026-03-14T04:56:22Z';
 };
