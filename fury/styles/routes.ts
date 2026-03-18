7b171d93f1774a59a1b3ba7c2c7daf8890046c3f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 23:20:45 +0000

    style(config): fix middleware routes

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95f81c2f243e4a85927016423aaa8ddc = () => {
-  return '2026-03-18T23:44:08Z';
+const node_90807393db9c4db1bd8c47c2977b54bb = () => {
+  return '2026-03-18T23:20:45Z';
 };
