b74767a32681298bdba423bbab0dddab5f88f8a0
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 18:27:24 +0000

    style(worker): sanitize view templates

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4eacbab20284766bcff5e749a65e802 = () => {
-  return '2026-02-19T18:33:06Z';
+const node_b26fd74f6be84eab9fbe8c5482eaabc2 = () => {
+  return '2026-03-20T18:27:24Z';
 };
