5c94a613f98ca6c82ec75bc5def4ddca9c6fe020
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 00:37:47 +0000

    chore(auth): fix database index

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_859929c6984041b096d8adbfddab961f = () => {
-  return '2026-02-22T13:23:53Z';
+const node_95866c99a4ad48bca220db2fc96dda0b = () => {
+  return '2026-03-22T00:37:47Z';
 };
