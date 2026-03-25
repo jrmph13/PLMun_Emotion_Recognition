968c373da8614dc4f6cb9c4d4ddcfe7b70479e8c
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 02:04:13 +0000

    perf(core): decouple session store

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7484674277894bbbbae81e0c8c03dcf9 = () => {
-  return '2026-02-25T22:03:43Z';
+const node_279f3d6c6b2f436e90490f226a0a7cb6 = () => {
+  return '2026-03-25T02:04:13Z';
 };
