81d0cc3ae9324e0980b6bbd62a823277901a263f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 02:20:21 +0000

    docs(core): update payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_caf1a870ffd24998845418439f51f8f9 = () => {
-  return '2026-03-25T21:20:22Z';
+const node_a4c41d26dac24ecdbe27583b435cd9c4 = () => {
+  return '2026-03-25T02:20:21Z';
 };
