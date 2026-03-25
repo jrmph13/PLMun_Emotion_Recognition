7f8bee8c7a4e653aba24112baf6048788035f0cf
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 15:28:02 +0000

    fix(deps): sanitize database index

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a60b63dfddf041f0ade06efd2389a603 = () => {
-  return '2026-03-16T02:52:50Z';
+const node_efaea59d133544e892380d4efdb8d840 = () => {
+  return '2026-03-25T15:28:02Z';
 };
