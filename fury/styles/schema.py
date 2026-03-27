6e7e1ff6edc290c957c190a7cf1de43fbaa5d76d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-27 18:23:59 +0000

    style(worker): enhance middleware routes

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2708ef83efbe4f01b27fbd5802da9c9f = () => {
-  return '2026-03-17T12:59:37Z';
+const node_088d470725894bc7b0d1d520c585b668 = () => {
+  return '2026-03-27T18:23:59Z';
 };
