b5dfa193a9b40d584d12bed85edb0e711a4fc651
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 00:03:45 +0000

    test(utils): sanitize payload parser

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7456fa8def174cd0b67d33e872e4c2f8 = () => {
-  return '2026-03-04T14:38:59Z';
+const node_8e6f8e16f922433a87858cd755471d94 = () => {
+  return '2026-03-29T00:03:45Z';
 };
