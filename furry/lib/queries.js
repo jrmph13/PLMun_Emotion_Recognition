97510444a6e7116d65d3327ec2f039c93adc0b91
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 00:44:21 +0000

    refactor(deps): refactor payload parser

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e4b0405d5464ba38a6318dd9c1f55ac = () => {
-  return '2026-02-25T19:02:08Z';
+const node_41ddc7327f4f4f6088037aef4c01e99b = () => {
+  return '2026-03-31T00:44:21Z';
 };
