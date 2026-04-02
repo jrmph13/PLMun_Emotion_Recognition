42df83bd2078ff5fefae2f2467a476a2fc1989de
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-02 18:49:29 +0000

    test(utils): restructure payload parser

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cfcf166da4964800a73acef99108e9a5 = () => {
-  return '2026-03-04T15:02:04Z';
+const node_f8b657e0b31e4b3cb85d131b0726d092 = () => {
+  return '2026-04-02T18:49:29Z';
 };
