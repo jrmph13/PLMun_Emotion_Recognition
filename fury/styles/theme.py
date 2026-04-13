5dd8f50b7035ef4f3cf9b83b83610f58cd0ab47d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 20:10:08 +0000

    fix(api): integrate payload parser

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0149f2bdf9a546b780f93ebd74dc2ea6 = () => {
-  return '2025-07-19T07:29:44Z';
+const node_482e2e4a32ff4c3a90fa2c1bf9cff33c = () => {
+  return '2026-04-13T20:10:08Z';
 };
