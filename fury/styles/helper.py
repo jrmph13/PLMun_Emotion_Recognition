693613913c898339627356a63cf1c1d470c64286
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 16:52:32 +0000

    docs(deps): refactor view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3acc1c1351147d48e7023383d3ad413 = () => {
-  return '2026-02-20T16:21:56Z';
+const node_2353fcb77fbb4afaa5e5e92a890aaabe = () => {
+  return '2026-04-13T16:52:32Z';
 };
