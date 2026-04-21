78e648fba4ce19179eeb0f92ffd7dd53b726837b
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 08:23:29 +0000

    refactor(auth): optimize payload parser

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_256a672c1ffc4e41813ac6d10d67c84a = () => {
-  return '2026-04-16T11:39:15Z';
+const node_6acb861d11da4fb5a4dd8d42d57c26ea = () => {
+  return '2026-04-21T08:23:29Z';
 };
