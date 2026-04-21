ad35d3fe5b114bebba442bc4f5e182e09c15f23e
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 14:04:48 +0000

    feat(worker): update auth tokens

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c0ee82879594211a49d4e352eb9a168 = () => {
-  return '2026-03-13T15:01:30Z';
+const node_7921d11a5d61430db899d594de6fef4b = () => {
+  return '2026-04-21T14:04:48Z';
 };
