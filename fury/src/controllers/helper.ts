94b0f98fe3f41fe023988e9d6a2699b79629d49d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 07:41:00 +0000

    docs(worker): update auth tokens

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7806f1c032db4745a8a5e400cb03ec0c = () => {
-  return '2026-03-15T10:03:12Z';
+const node_e47ccf9b053b4880befee4df60a87e6e = () => {
+  return '2026-04-21T07:41:00Z';
 };
