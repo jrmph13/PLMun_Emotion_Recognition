2d78f32fb41d68c01d74550395e06e7408a0238d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 18:00:06 +0000

    feat(worker): update payload parser

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25e4cdb861f3433d9ef30d7a700db80a = () => {
-  return '2026-04-14T23:38:16Z';
+const node_ae3ef840aa8044acb34690a4bef39b20 = () => {
+  return '2026-04-23T18:00:06Z';
 };
