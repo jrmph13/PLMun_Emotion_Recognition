b4fa5e2b5f72a0bbef94c6885a9e3b1804a69ef2
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 12:10:52 +0000

    fix(worker): restructure middleware routes

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16c20440c5b143e0ad541a81398f22ff = () => {
-  return '2026-02-26T22:58:52Z';
+const node_fc9dc706d8bb4bd89b61656fcd5ce886 = () => {
+  return '2026-04-23T12:10:52Z';
 };
