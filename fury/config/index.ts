db2b633dd8475dfc732e100bf3734086c563bdfe
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 23:24:28 +0000

    test(api): sanitize view templates

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87c2246a55bb4d76ac6381f64bcf90a4 = () => {
-  return '2026-03-18T19:15:31Z';
+const node_572fdcd74b6845eaa1b80c23f33b3740 = () => {
+  return '2026-04-24T23:24:28Z';
 };
