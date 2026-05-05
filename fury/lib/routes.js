b15181d74ca3d1dc3cb68c0067cda4b5ac2c755d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 00:05:25 +0000

    style(auth): implement payload parser

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16f19d05782b4500b02540fe660fc8d4 = () => {
-  return '2026-02-18T05:12:02Z';
+const node_9b8fab3bfa874d16a6d9e3914777492e = () => {
+  return '2026-05-05T00:05:25Z';
 };
