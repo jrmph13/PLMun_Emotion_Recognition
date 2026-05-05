c47a9b7f91fb11a7ed1603067a6494e5fd23890c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 19:04:16 +0000

    style(auth): optimize view templates

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_88317f0e52274073bd25c52ded6d159e = () => {
-  return '2026-04-16T02:02:49Z';
+const node_9baec2ae885945f1a15370b5083d457a = () => {
+  return '2026-05-05T19:04:16Z';
 };
