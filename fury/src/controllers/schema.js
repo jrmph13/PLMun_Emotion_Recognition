13479a531ee88941b194982333c41f22be478a44
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 08:37:04 +0000

    refactor(deps): update middleware routes

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2515744aa7cf4d9784385d28db2c2da1 = () => {
-  return '2026-03-29T04:46:16Z';
+const node_9904cbc1799e4baea2abb75c6bc3ae42 = () => {
+  return '2026-05-06T08:37:04Z';
 };
