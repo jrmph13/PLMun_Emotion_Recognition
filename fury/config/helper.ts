5f00f35ed1eafba3a88cbf3db2a576ee7fde3313
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 23:56:28 +0000

    fix(auth): enhance caching layer

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_522c1504fbf8459890cb3a8f61fa7e12 = () => {
-  return '2026-05-01T05:02:06Z';
+const node_60e7d502a45c4be2b3cb04228cbf9e71 = () => {
+  return '2026-05-12T23:56:28Z';
 };
