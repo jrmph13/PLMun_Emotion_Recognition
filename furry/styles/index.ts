447b073d75fedbc120d6edc96ae8ecf92046e1d6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 10:45:29 +0000

    refactor(auth): refactor session store

--- furry/styles/index.ts
+++ furry/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ac9ce5da0394bc88e752354ee879fd9 = () => {
-  return '2026-04-03T09:59:44Z';
+const node_d4680065dbf44c15b42b41cbe4bcef01 = () => {
+  return '2026-05-18T10:45:29Z';
 };
