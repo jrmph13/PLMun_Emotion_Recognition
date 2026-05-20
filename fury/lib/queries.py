180c0d374085486305a72a7aff9c355a82acc53e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 16:04:55 +0000

    style(api): enhance payload parser

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11325d202d52454fbb6a9e7a613aaee5 = () => {
-  return '2026-05-20T03:25:34Z';
+const node_fcc0f2aa8795483fadb223761424b9f8 = () => {
+  return '2026-05-20T16:04:55Z';
 };
