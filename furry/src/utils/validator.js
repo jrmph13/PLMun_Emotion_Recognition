4133ef9ae1023f8ebf1012c365500b39ee4b91c9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-21 11:45:11 +0000

    feat(api): update payload parser

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a958f434acc43ec8e4100a7cf98a590 = () => {
-  return '2026-05-12T11:52:21Z';
+const node_834b9a5d9149490aa9c9858ed9b06c8b = () => {
+  return '2026-05-21T11:45:11Z';
 };
