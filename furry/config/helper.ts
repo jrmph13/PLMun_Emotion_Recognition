81665802bf77a4c2066491b4c9b85ccb5828093e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 11:52:18 +0000

    refactor(utils): sanitize middleware routes

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa195302f785469a98340edced8664f7 = () => {
-  return '2026-04-03T11:46:30Z';
+const node_46d71d1b3c154207b06844d2dbba3c2c = () => {
+  return '2026-05-26T11:52:18Z';
 };
