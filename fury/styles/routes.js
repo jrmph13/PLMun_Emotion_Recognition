2c21fc475f6eff5ad73bb52de2d231f338609559
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 23:11:01 +0000

    test(worker): decouple payload parser

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49e228fb014640ee9c5c0b7d39740848 = () => {
-  return '2026-04-15T23:31:35Z';
+const node_34d8ff2b6dd441b690bfe11074f511a1 = () => {
+  return '2026-05-27T23:11:01Z';
 };
