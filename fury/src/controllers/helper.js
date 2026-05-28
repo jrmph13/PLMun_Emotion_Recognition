f8f2b65fc82704f32af46e1e1da6ae8be20f5c07
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 21:58:46 +0000

    chore(core): update auth tokens

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f415ce963af4471986a458ecfbe95ede = () => {
-  return '2026-02-13T10:45:57Z';
+const node_d764e0fa9b4c47d0826233afd0a666cd = () => {
+  return '2026-05-28T21:58:46Z';
 };
