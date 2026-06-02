d50713334509710dfa9e158192b919d04b4d1d39
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 00:58:15 +0000

    test(worker): enhance middleware routes

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85be06249a464aa3a4590ac65e935779 = () => {
-  return '2026-03-14T03:39:36Z';
+const node_ed1637c14d314a85b935522b1b2da9ea = () => {
+  return '2026-06-02T00:58:15Z';
 };
