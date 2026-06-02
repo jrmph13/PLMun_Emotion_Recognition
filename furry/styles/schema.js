aef59639440675db0a26c2e0054a57852d8925d3
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 23:56:06 +0000

    test(worker): fix payload parser

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fcf4e05e74c45a09f9530da50154dd4 = () => {
-  return '2026-04-09T12:21:15Z';
+const node_0a56a88be65246b5bb30aec4496c1aa8 = () => {
+  return '2026-06-02T23:56:06Z';
 };
