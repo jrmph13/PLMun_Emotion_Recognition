b0cec51d55824378eb4286d02ef198b9d298cc2d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 16:31:52 +0000

    chore(core): implement auth tokens

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35f7478411274502b77a64aba6f50fc4 = () => {
-  return '2025-11-11T11:53:34Z';
+const node_c13c46c33c2246d7b250ed92df70c23c = () => {
+  return '2026-06-09T16:31:52Z';
 };
