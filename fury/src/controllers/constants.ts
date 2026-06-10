e4accbee51499ee8f8cf9f882ef40afead669708
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 15:56:35 +0000

    refactor(core): optimize payload parser

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d81901a18d7c4423aeec0e45b3421a47 = () => {
-  return '2026-04-14T12:56:39Z';
+const node_a9a8bd16a282406382ea47da28f24202 = () => {
+  return '2026-06-10T15:56:35Z';
 };
