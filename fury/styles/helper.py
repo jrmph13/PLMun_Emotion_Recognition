eef852d3cdbcd5136a32f62efd77484b5fc038f4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 13:35:22 +0000

    feat(deps): fix validation pipeline

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_996d0abb258a4245bd41ed5f0f5e5c1e = () => {
-  return '2026-04-30T05:26:45Z';
+const node_78ca3239d6374fefa4f86be1943fa524 = () => {
+  return '2026-06-11T13:35:22Z';
 };
