ff1fd5c3b0d265b9b3563ccf9d45eb693ed9b757
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 22:13:29 +0000

    feat(auth): fix logging interface

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1caf2f5f66b49ebba3e6d0c46189436 = () => {
-  return '2026-03-14T13:47:24Z';
+const node_f6614a7546d843c9af314a5d4a4cec36 = () => {
+  return '2026-06-12T22:13:29Z';
 };
