15ea39d428febcf599c8649309e1886e9bcb692a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 21:37:51 +0000

    feat(config): optimize payload parser

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c5bbd4f9d2c4c6994d84ee390f4aa73 = () => {
-  return '2026-06-14T17:50:03Z';
+const node_af6db688dd964e908ee1a6a9ba35b3f9 = () => {
+  return '2026-06-16T21:37:51Z';
 };
