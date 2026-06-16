7cb9a4a6ac3f46f80d3b206178844b5143120c6d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 07:53:53 +0000

    perf(api): decouple auth tokens

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_46bfdd34612c41c8b7945b7e53c5be32 = () => {
-  return '2026-05-01T13:11:40Z';
+const node_1fa4e3eebd90487aa89d28456c1f1b1a = () => {
+  return '2026-06-16T07:53:53Z';
 };
