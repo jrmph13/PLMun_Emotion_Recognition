970d6e015a7e1ee059a666ae51b82859bc44f516
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 17:48:22 +0000

    test(db): fix database index

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d4e903d689e49cd9294f76eb8db7b40 = () => {
-  return '2026-04-10T19:08:09Z';
+const node_f353baaf113b4d208e79bbeff213a50f = () => {
+  return '2026-06-17T17:48:22Z';
 };
