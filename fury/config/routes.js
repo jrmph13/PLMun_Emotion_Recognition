0252d7784e6b61eb72fa876878305b0e1c20a2e8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 20:21:42 +0000

    perf(config): sanitize middleware routes

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5370ac09a4d4a72bdc8ecd39050d9d1 = () => {
-  return '2026-04-13T17:02:20Z';
+const node_4a098b91d97f46f4a81315740dd4b261 = () => {
+  return '2026-06-18T20:21:42Z';
 };
