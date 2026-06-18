13688735a3da70778758a6c80e1e3f7799cf90ca
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 22:32:01 +0000

    test(ui): optimize payload parser

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f66f4dfc41849de9edff36df35c248c = () => {
-  return '2026-06-11T15:01:22Z';
+const node_54424025fa9a41148b35b1ff8749b3df = () => {
+  return '2026-06-18T22:32:01Z';
 };
