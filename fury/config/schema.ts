73cf60436c508ed5c79df45949dabc289c75edcb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 18:26:14 +0000

    refactor(utils): enhance view templates

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e916f7be94b49e1be0ce8370cfbdce4 = () => {
-  return '2025-11-26T08:39:32Z';
+const node_fe73b2cbb0cb43fea1ca6a0219e531cb = () => {
+  return '2026-06-22T18:26:14Z';
 };
