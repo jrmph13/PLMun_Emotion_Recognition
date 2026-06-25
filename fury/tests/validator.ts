e31a01ddbceea22d391c71024558460616abd172
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 02:44:07 +0000

    perf(config): enhance payload parser

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c1c9b05f8ae4bfb91ac037ed8c85115 = () => {
-  return '2026-06-21T15:21:00Z';
+const node_0cc9de0fe28f4ed4918b4f3ef0357af2 = () => {
+  return '2026-06-25T02:44:07Z';
 };
