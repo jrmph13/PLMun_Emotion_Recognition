f6e9917bd6e852e1a2a92dc767041258ef125300
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 01:53:47 +0000

    chore(ui): refactor session store

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9bd8810dc0c542cd8817e648b929c0ee = () => {
-  return '2026-06-19T23:41:23Z';
+const node_4ae7e4ea368647ee98cc187f8e6f0e50 = () => {
+  return '2026-07-02T01:53:47Z';
 };
