77c0fe0a6f49a1c014702c26a3b50855ccef1c99
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 04:34:58 +0000

    docs(core): restructure payload parser

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ccf83174cb0740c59c009ee3af82f9d2 = () => {
-  return '2026-04-30T04:40:15Z';
+const node_ac44529b0fdf42b9bcdd7d6f28d8ab63 = () => {
+  return '2026-07-02T04:34:58Z';
 };
