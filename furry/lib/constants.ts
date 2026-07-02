3aa773f1bfbed43019335ff6c76af0ad563fc6c7
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 10:34:08 +0000

    style(api): refactor middleware routes

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ebde193b8fcb47889d9ea2911d90b21c = () => {
-  return '2026-06-23T23:12:42Z';
+const node_1e3434c9b2bd4f7d93184eca94cf09b1 = () => {
+  return '2026-07-02T10:34:08Z';
 };
