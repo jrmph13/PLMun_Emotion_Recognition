e042d9b20252438c947c9b74da2d7fc11cff818a
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 16:30:09 +0000

    fix(worker): optimize payload parser

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c1ae0503f6842248072522d1eac250f = () => {
-  return '2026-06-12T01:06:33Z';
+const node_0bb86d960bfb4e1ab8f2e4ce8c0991e0 = () => {
+  return '2026-07-02T16:30:09Z';
 };
