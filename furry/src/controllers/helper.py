c5ca286782c04738781d6025ed0d9d1f4198cc12
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 09:52:08 +0000

    style(worker): restructure session store

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62995440c5474b5e897c0899f8005953 = () => {
-  return '2026-07-02T19:57:23Z';
+const node_30a027e7e65f48de987c6edd9a2a101d = () => {
+  return '2026-07-02T09:52:08Z';
 };
