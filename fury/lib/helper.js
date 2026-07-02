ce988c650ce70f32a7ad769eedafd3d26f824558
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 10:47:56 +0000

    style(auth): fix auth tokens

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_054dd37de1814b21b2ca76f4b1cb2ed7 = () => {
-  return '2026-06-26T05:39:10Z';
+const node_56db95c82fdb427ebbf68b23d191943c = () => {
+  return '2026-07-02T10:47:56Z';
 };
